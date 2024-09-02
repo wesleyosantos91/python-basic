d22b37b702251b44ff1cdce09ff3c90698650b2b
Wesley Oliveira Santos <wesleyosantos91@gmail.com>
2024-09-02 11:42:39 -0300

    feat: implemantado teste unitarios

--- aws/src/s3/s3_service.py
+++ aws/src/s3/s3_service.py
@@ -3,7 +3,9 @@ import os
 import boto3
 from aws_lambda_powertools import Logger
 
-env = os.getenv("ENVIROMENT")
+env = os.getenv("ENVIRONMENT")
+BYTES_PER_MB = 1024 * 1024
+TEN_MB_IN_BYTES = 10 * BYTES_PER_MB
 
 
 class S3Service:
@@ -18,6 +20,13 @@ class S3Service:
     def get_object_s3(self, bucket, key):
         try:
             self.__logger.info(f"Getting object from bucket {bucket} with key {key}")
+
+            head_response = self.__s3_client.head_object(Bucket=bucket, Key=key)
+
+            object_size = head_response['ContentLength']
+
+            if object_size > TEN_MB_IN_BYTES:
+                raise Exception("Object size is greater than 10MB")
 
             response = self.__s3_client.download_file(Bucket=bucket, Key=key, Filename=key)
             self.__logger.info(f"Object from bucket {bucket} with key {key} retrieved")
--- aws/test/s3/test_s3_service.py
+++ aws/test/s3/test_s3_service.py
@@ -9,23 +9,42 @@ class TestS3Service(unittest.TestCase):
     def test_get_object_s3_success(self, mock_logger, mock_boto_client):
         mock_s3_client = MagicMock()
         mock_boto_client.return_value = mock_s3_client
+        mock_s3_client.head_object.return_value = {'ContentLength': 5 * 1024 * 1024}  # 5 MB
         mock_s3_client.download_file.return_value = None
 
         s3_service = S3Service()
         response = s3_service.get_object_s3('test-bucket', 'test-key')
 
+        mock_s3_client.head_object.assert_called_once_with(Bucket='test-bucket', Key='test-key')
         mock_s3_client.download_file.assert_called_once_with(Bucket='test-bucket', Key='test-key', Filename='test-key')
         self.assertIsNone(response)
 
     @patch('boto3.client')
     @patch('aws_lambda_powertools.Logger')
-    def test_get_object_s3_failure(self, mock_logger, mock_boto_client):
+    def test_get_object_s3_failure_due_to_size(self, mock_logger, mock_boto_client):
         mock_s3_client = MagicMock()
         mock_boto_client.return_value = mock_s3_client
+        mock_s3_client.head_object.return_value = {'ContentLength': 15 * 1024 * 1024}  # 15 MB
+
+        s3_service = S3Service()
+        with self.assertRaises(Exception) as context:
+            s3_service.get_object_s3('test-bucket', 'test-key')
+
+        self.assertEqual(str(context.exception), "Object size is greater than 10MB")
+        mock_s3_client.head_object.assert_called_once_with(Bucket='test-bucket', Key='test-key')
+        mock_s3_client.download_file.assert_not_called()
+
+    @patch('boto3.client')
+    @patch('aws_lambda_powertools.Logger')
+    def test_get_object_s3_failure_due_to_download_error(self, mock_logger, mock_boto_client):
+        mock_s3_client = MagicMock()
+        mock_boto_client.return_value = mock_s3_client
+        mock_s3_client.head_object.return_value = {'ContentLength': 5 * 1024 * 1024}  # 5 MB
         mock_s3_client.download_file.side_effect = Exception('Error')
 
         s3_service = S3Service()
         with self.assertRaises(Exception):
             s3_service.get_object_s3('test-bucket', 'test-key')
 
+        mock_s3_client.head_object.assert_called_once_with(Bucket='test-bucket', Key='test-key')
         mock_s3_client.download_file.assert_called_once_with(Bucket='test-bucket', Key='test-key', Filename='test-key')
